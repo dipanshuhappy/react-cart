d6f087bc48fa0a146f39e19f6486212f182edd41
dipanshu <dipanshuhappy@gmail.com>
2020-09-25 19:35:04 +0100

    sup

---
+++ precache-manifest.d43d4aa081be9ff3b436e7909ca31e35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75d20e0aa90f5ea59bb1",
+    "url": "/static/css/main.585905d0.chunk.css"
+  },
+  {
+    "revision": "75d20e0aa90f5ea59bb1",
+    "url": "/static/js/main.b07ab42a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "153242236ec563e67d46",
+    "url": "/static/js/2.8c15e647.chunk.js"
+  },
+  {
+    "revision": "ae7651cde2ffd283a94ddd3953b928a1",
+    "url": "/index.html"
+  }
+];
